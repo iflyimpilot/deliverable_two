e93511f8d54e6bf66316e0eb7f6ca50aaa2b846d
louie <iflyimpilot@gmail.com>
2019-06-16 23:09:02 -0400

    Updated index.html and scripts

--- js/script.js
+++ js/script.js
@@ -1,37 +1,43 @@
+var eventType = prompt("What type of event are you attending?","Casual?  Semi-formal?  Formal?");
 
-      var result = document.querySelector('p');
-      var eventTypeSelect = document.getElementById('eventType')
-		  var weatherSelect = document.getElementById('tempFahr')
+var tempFahr = prompt("What is today's temperature?","Enter today's temperature.");
+console.log(tempFahr + " degrees Fahrenheit.");
 
-
-    
-      eventTypeSelect.onchange = setEventType;
-      weatherSelect.onchange = setWeatherType;
+var result = ("It is " + tempFahr + " degrees and you are going to a " + eventType + " event. ");
 
 
-       function setEventType() {
-        var choice = eventTypeSelect.value;
-        if(choice === 'casual') {
-          result.textContent = 'Wear something comfy.';
-          console.log(result.textContent);
-        } else if(choice === 'semi-formal') {
-          result.textContent = 'Wear a polo.';
-          console.log(result.textContent);
-        } else if(choice === 'formal') {
-          result.textContent = 'Wear a suit.';
-          console.log(result.textContent);
-        }
-      }
-      	function setWeatherType() {
-        var choice = weatherSelect.value;
-        if(choice === 'under54') {
-          result.textContent = 'Wear a coat.';
-          console.log(result.textContent);
-        } else if(choice === '54-70') {
-          result.textContent = 'Wear a jacket.';
-          console.log(result.textContent);
-        } else if(choice === 'above70') {
-          result.textContent = 'No jacket needed.';
-          console.log(result.textContent);
-        }
-      }
+//What to wear based on Event
+if (eventType === "Casual") {
+	result = result + "Wear something comfy "
+console.log("Wear something comfy.");
+} else if (eventType === "Semi-formal") {
+	result = result + "Wear a polo "
+console.log("Wear a polo.");
+} else if (eventType === "Formal") {
+	result = result + "Wear a suit "
+console.log("Wear a suit.");
+}
+
+
+
+
+
+
+//What to wear based on Temperature
+if (tempFahr < 54) {
+console.log ("Wear a coat.");
+	result = result + "and a coat. "
+console.log (result);
+
+} else if (tempFahr <= 54 || tempFahr <= 70) {
+console.log ("Wear a jacket.");
+	result = result + "and a jacket. "
+console.log (result);
+} else if (tempFahr >= 70) {
+console.log ("Wear no jacket.");
+	result = result + "and no jacket. "
+}
+
+
+console.log(result)
+document.getElementById("result").innerHTML = result
